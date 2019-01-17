a8b5e583704c0265e80b1d23b6e6b074a2c9f082
shivhek25 <shivamsinghal0610@gmail.com>
2019-01-17 22:27:58 +0530

    Logical error solved

--- tasks/task_1/task1_code.cpp
+++ tasks/task_1/task1_code.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-using namespace std;
 
 typedef long long int ll;
 int arr[1000005];
@@ -54,7 +53,7 @@ int main()
     if(w < 0) w = 0;
 
     dmn = min(d1,d2);
-    w += (pre[dm] - pre[dmn]);
+    w += (pre[dm-1] - pre[dmn-1]);
     (dmn == d1) ? w -= u1 : w -= u2;
     if(w < 0) w = 0;
 
