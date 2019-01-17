32109a7d27b00fff90fdf21f62d343813dd2f473
ak0005 <46166186+ak0005@users.noreply.github.com>
2019-01-18 02:11:40 +0530

    Update task1_code.cpp

--- tasks/task_1/task1_code.cpp
+++ tasks/task_1/task1_code.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-
+using namespace std;
 typedef long long int ll;
 int arr[1000005];
 ll pow_mod(ll a, ll b, ll M)
@@ -22,7 +22,7 @@ void sieve(int n)
     {
         if(arr[p] == p)
         {
-            for(i = p;i< = n;i += p)
+            for(i = p;i<= n;i += p)
             {
                 arr[i] = p;
             }
@@ -34,31 +34,31 @@ int main()
 {
     #ifndef ONLINE_JUDGE
     // Assume input.txt and output.txt suitably exists
+    freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
 
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     ll i,w,h,u1,u2,d1,d2,dm,dmn,pre[100001]={0};
     for(i=1;i<100001;i++)
     {
         pre[i] += (pre[i - 1] + i);
     }
-    std::cin>>w>>h;
-    std::cin>>u1>>d1;
-    std::cin>>u2>>d2;
+    cin>>w>>h;
+    cin>>u1>>d1;
+    cin>>u2>>d2;
 
-    dm = std::max( d1, d2);
+    dm = max( d1, d2);
     w += ( pre[h] - pre[dm-1] );
     (dm == d1) ? w -= u1 : w -= u2;
     if(w < 0) w = 0;
 
-    dmn = std::min(d1,d2);
-    w += (pre[dm] - pre[dmn]);
-
+    dmn = min(d1,d2);
+    w += (pre[dm-1] - pre[dmn-1]);
     (dmn == d1) ? w -= u1 : w -= u2;
     if(w < 0) w = 0;
 
     w += (pre[dmn - 1]);
-    std::cout<<w<<" ";
+    cout<<w<<" ";
     return 0;
 }
