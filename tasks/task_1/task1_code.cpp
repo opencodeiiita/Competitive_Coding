4d5780f6e478c8d9b7d0aea794c3b66dedb1a39e
Deepanshu Maheshwari <42608451+ThunderrZero@users.noreply.github.com>
2019-01-17 16:03:36 +0530

    Update line number 53

--- tasks/task_1/task1_code.cpp
+++ tasks/task_1/task1_code.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-
+using namespace std;
 typedef long long int ll;
 int arr[1000005];
 ll pow_mod(ll a, ll b, ll M)
@@ -22,7 +22,7 @@ void sieve(int n)
     {
         if(arr[p] == p)
         {
-            for(i = p;i< = n;i += p)
+            for(i = p;i<= n;i += p)
             {
                 arr[i] = p;
             }
@@ -38,17 +38,19 @@ int main()
     #endif
 
     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     ll i,w,h,u1,u2,d1,d2,dm,dmn,pre[100001]={0};
     for(i=1;i<100001;i++)
     {
-        pre[i] += (pre[i - 1] + i);
+        pre[i] = (pre[i - 1] + i);
     }
     cin>>w>>h;
     cin>>u1>>d1;
     cin>>u2>>d2;
 
     dm = max( d1, d2);
-    w += ( pre[h] - pre[dm-1] );
+    w += ( pre[h] - pre[dm] );
     (dm == d1) ? w -= u1 : w -= u2;
     if(w < 0) w = 0;
 
@@ -57,7 +59,7 @@ int main()
     (dmn == d1) ? w -= u1 : w -= u2;
     if(w < 0) w = 0;
 
-    w += (pre[dmn - 1]);
+    w += (pre[dmn]);
     cout<<w<<" ";
     return 0;
 }
