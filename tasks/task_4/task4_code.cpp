28134e151bcf1b5fc83622ae4f4cb74b0b732787
eunoia_1729 <bkumarraju@gmail.com>
2019-01-30 03:18:54 +0530

    Adding task4

---
+++ tasks/task_4/task4_code.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PI 3.141592
+#define ll long long int
+#define N 100000
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+	ll s[N],i,j;
+	s[0]=s[1]=1;
+	for(i=2;i<N;i++)
+	    s[i]=1;
+	for(i=2;i<N;i++) {
+	    if(s[i]==1) {
+	        for(j=i+i;j<N;j+=i)
+	            s[j]=0;
+	    }
+	}
+	vector <ll> primes;
+	i=0;
+	while(i<N) {
+	    if(s[i]==1)
+	        primes.push_back(i);
+	    (primes.size()<50000)?i++:break;
+	}
+	vector <double> a;
+	ll tem;
+	for(i=0;i<primes.size();i++) {
+	    tem=PI/primes[i];
+	    (i==0)?a.push_back(tem):a.push_back(a[i-1]+tem);
+	}
+	ll T,n;
+	cin>>T;
+	while(T--) {
+	    cin>>n;
+	    cout<<a[n-1]<<"\n";
+	}
+	return 0;
+}
---
+++ tasks/task_4/task4_question.md
@@ -0,0 +1,17 @@
+T testcases.
+In each test case, a number <b>N</b> in given. Print corresponding <b>A</b> whose value is equal to X. </br>
+
+X = pi/p<sub>1</sub> + pi/p<sub>2</sub> + pi/p<sub>3</sub> + pi/p<sub>4</sub> + ...... + pi/p<sub>N</sub> </br>
+
+where p<sub>1</sub>, p<sub>1</sub>, p<sub>1</sub>, .... p<sub>n</sub> are first N primes.
+</br>
+
+## INPUT </br>
+first line - T. followed by T lines. i-th line is N for i-th testcase
+</br>
+## OUTPUT </br>
+T lines. i-th line is A for i-th testcase. Answer is considered correct if abs diff does not exceed 10 power 8. </br>
+
+## CONSTRAINTS </br>
+1<= T<= 10^4 </br>
+1<= N-th prime <= 50000 </br>
